249fb1d2c9a48c3ad4f7c7786cbf0000fa56164a
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2024-04-08 19:02:34 +0530

    update

--- cse_machine/apply_un.py
+++ cse_machine/apply_un.py
@@ -4,7 +4,7 @@ def apply_unary(cse_machine, rator_e, unop):
     unary_operators = {
             "Print": lambda cse_machine, operand: apply_print(cse_machine, operand),
             "Isstring": lambda cse_machine, operand: isinstance(operand.value, str),
-            "Isinteger": lambda cse_machine, operand: isinstance(operand.value, int),
+            "Isinteger": lambda cse_machine, operand: isinstance(operand.value, int) and not isinstance(operand.value, bool),
             "Istruthvalue": lambda cse_machine, operand: isinstance(operand.value, bool),
             "Isfunction": lambda cse_machine, operand: "lambda"==operand.type,
             "Null": lambda cse_machine, operand: operand.type == "nil",
@@ -12,7 +12,7 @@ def apply_unary(cse_machine, rator_e, unop):
             "Order": lambda cse_machine, operand: len(operand.value) if isinstance(operand.value, list) else cse_machine._error_handler.handle_error("CSE : Invalid unary operation"),
             "Stern": lambda cse_machine, operand: apply_stern(cse_machine, operand.value),
             "Stem": lambda cse_machine, operand: apply_stem(cse_machine, operand.value),
-            "ItoS": lambda cse_machine, operand: str(operand.value),
+            "ItoS": lambda cse_machine, operand: str(operand.value) if isinstance(operand.value, int) and not isinstance(operand.value, bool) else cse_machine._error_handler.handle_error("CSE : Invalid unary operation"),
             "neg": lambda cse_machine, operand: -operand.value if isinstance(operand.value, int) else cse_machine._error_handler.handle_error("CSE : Invalid unary operation"),
             "not": lambda cse_machine, operand: not operand.value if isinstance(operand.value, bool) else cse_machine._error_handler.handle_error("CSE : Invalid unary operation"),
         }
